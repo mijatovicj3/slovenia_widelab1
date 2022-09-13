2018e689114e9ee6312bf916f3273e5addf51f20
mijatovicj3 <98957665+mijatovicj3@users.noreply.github.com>
2022-09-13 12:32:17 +0200

    Add files via upload

---
+++ updated_phase1.cpp
@@ -0,0 +1,69 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"enter a number between 3 and 45"<<endl;
+   //unos
+
+   int x;
+   cin>>x;
+   
+   if(x>=3 && x<=45)
+   { 
+   x=(x/2);
+   int z=1;
+  
+   //gornja polovina romba
+    
+   for ( int i=0; i<=x; i++)
+   {
+      for (int j = x; j > i; j--)
+      {
+      cout<<" "; 
+      }
+
+    cout<<"*";  
+    
+    if ( i>0)
+    {
+      for ( int k=1; k<=z; k++)
+      {
+        cout<<" ";
+      }
+      z+=2;
+      cout<<"*";
+    }
+    cout<<endl; 
+  }
+  z-=4;
+  
+   //donja polovina romba
+
+  for (int i=0; i<=x-1; i++)
+  {
+    for (int j=0; j<=i; j++)
+    {
+      cout<<" ";
+    }
+
+    cout<<"*";
+
+    for (int k=1; k<=z; k++)
+    {
+      cout<<" ";
+    }
+    z-=2;
+
+    if (i!=x-1)
+    {
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+  }
+  else{
+  	cout<<"invalid number try again";
+  }
+return 0;
+}
